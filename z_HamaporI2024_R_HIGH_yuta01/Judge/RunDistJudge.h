694ebd9969f3edba02a30f993440d7d897f4fab4
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-11-21 09:59:29 +0900

    Add files via upload

---
+++ z_HamaporI2024_R_HIGH_yuta01/Judge/RunDistJudge.h
@@ -0,0 +1,29 @@
+﻿#ifndef ___CLASS_RUNDISTJUDGE
+#define ___CLASS_RUNDISTJUDGE
+
+#include "Judge.h"
+#include "RunDistDetect.h"
+
+class RunDistJudge : public Judge {
+public:
+	RunDistJudge(RunDistDetect* rundistdetect);
+	bool judge();
+
+private:
+	RunDistDetect *mRunDistDetect;
+
+	inline static constexpr float target_dist[17] = {
+		80, 2700, 600, 1500, 500, 550,
+		300, 100, 350, 150, 400, 160, 200,
+		//////
+		1000, 250, 1900, 250,
+	};
+
+	static int8_t size;
+
+	int8_t cnt;
+
+	float current_dist;
+};
+
+#endif // ___CLASS_RUNDISTJUDGE
