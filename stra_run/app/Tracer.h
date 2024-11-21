3f7da0d38f8762d52790d4744aa656ee6888f1fd
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-11-21 09:37:28 +0900

    Add files via upload

---
+++ stra_run/app/Tracer.h
@@ -0,0 +1,26 @@
+#include "Motor.h"       // <1>
+#include "ColorSensor.h" // <2>
+#include "SonarSensor.h"
+#include "GyroSensor.h"
+
+using namespace ev3api;  // <4>
+
+class Tracer {  // <1>
+public:
+  Tracer();
+  bool run();       // <2>
+  void terminate();
+
+private:
+  Motor leftWheel;
+  Motor rightWheel;
+  ColorSensor colorSensor; // <3>
+  SonarSensor sonarSensor;
+  GyroSensor gyroSensor;
+#ifndef MAKE_RASPIKE
+  const int8_t pwm = (Motor::PWM_MAX) / 6;
+#else
+  const int8_t pwm = 70;
+#endif
+
+};
