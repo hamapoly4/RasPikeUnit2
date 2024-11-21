984ff7927cbd3603e3412823297ecbe40dc5f30d
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-11-21 09:10:21 +0900

    Add files via upload

---
+++ 01_double_loop_NEO_new/SpikeCon/LineTraceRun.cpp
@@ -0,0 +1,26 @@
+﻿#include "LineTraceRun.h"
+#include <stdio.h>
+
+using namespace ev3api;
+
+LineTraceRun::LineTraceRun(unsigned char edge, Motor& leftmotor, Motor& rightmotor, PDcon* pdconcolor)
+	: medge(edge), mLeftMotor(leftmotor), mRightMotor(rightmotor), mPDconColor(pdconcolor), Run(PWM)
+{
+	;
+}
+
+void LineTraceRun::run()
+{
+	mturn = mPDconColor->getTurn();
+	if (medge == RIGHT)
+	{
+		mLeftMotor.setPWM(mfix_pwm - mturn);
+		mRightMotor.setPWM(mfix_pwm + mturn);
+	}
+	else if (medge == LEFT)
+	{
+		mLeftMotor.setPWM(mfix_pwm + mturn);
+		mRightMotor.setPWM(mfix_pwm - mturn);
+		printf("LineTrace Left Edge!!\n");
+	}
+}
