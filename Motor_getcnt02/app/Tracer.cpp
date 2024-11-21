0874486f54d9ba41bd1004e2d706422603339564
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-11-21 09:30:07 +0900

    Add files via upload

---
+++ Motor_getcnt02/app/Tracer.cpp
@@ -0,0 +1,35 @@
+#include "Tracer.h" // <1>
+#include <stdio.h>
+
+Tracer::Tracer():
+  leftWheel(PORT_C), rightWheel(PORT_B), colorSensor(PORT_2), sonarSensor(PORT_3) { // <2>
+  }
+
+void Tracer::terminate() {
+  leftWheel.stop();  // <1>
+  rightWheel.stop();
+}
+
+void Tracer::run() {
+  static bool flag = false;
+  int16_t dist;
+
+  printf("-------------------once-------------------\n");
+  printf("leftWheel.getCount  : %d\n", leftWheel.getCount());
+  printf("rightWheel.getCount : %d\n", rightWheel.getCount());
+  
+  if (ev3_button_is_pressed(ENTER_BUTTON))
+  {
+    flag = true;
+  }
+  else
+  {
+    if (flag == true)
+    {
+      flag = false;
+      printf("*******************twice*******************\n");
+      printf("leftWheel.getCount  : %d\n", leftWheel.getCount());
+      printf("rightWheel.getCount : %d\n", rightWheel.getCount());
+    }
+  }
+}
