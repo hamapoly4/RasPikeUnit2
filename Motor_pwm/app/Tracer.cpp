a2dd5789755641cd4eb618f91d06ab4f14716909
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-11-21 09:30:30 +0900

    Add files via upload

---
+++ Motor_pwm/app/Tracer.cpp
@@ -0,0 +1,16 @@
+#include "Tracer.h" // <1>
+#include <stdio.h>
+
+Tracer::Tracer():
+  leftWheel(PORT_C), rightWheel(PORT_B), colorSensor(PORT_2), sonarSensor(PORT_3) { // <2>
+  }
+
+void Tracer::terminate() {
+  leftWheel.stop();  // <1>
+  rightWheel.stop();
+}
+
+void Tracer::run() {
+  leftWheel.setPWM(45);
+  rightWheel.setPWM(45);
+}
